2c762c954097bbafe6bc6de2dc8a4d5fa2f52d83
dcereijodo <dcereijo@digitalorigin.com>
2019-06-02 12:59:25 +0200

    created structure for first test

---
+++ tests/test_gsheet_loader.py
@@ -0,0 +1,17 @@
+import unittest
+
+from tap_gsheets.gsheet_loader import GSheetsLoader
+
+
+class TestSum(unittest.TestCase):
+    def test_authentication(self):
+        """
+        Test that we are able to authenitcate provided some valid configs
+        are passed
+
+        TO BE DEFINED
+        """
+
+
+if __name__ == '__main__':
+    unittest.main()
