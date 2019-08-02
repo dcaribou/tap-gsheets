315bd8989d7889ebadf5c49d54a3f2c8caf3774c
dcereijodo <dcereijo@digitalorigin.com>
2019-08-02 20:51:01 +0200

    base64 encode multiline key

--- tests/integration/test_gsheet_loader.py
+++ tests/integration/test_gsheet_loader.py
@@ -3,13 +3,14 @@ import unittest
 from tap_gsheets.gsheet_loader import GSheetsLoader
 import json
 import os
+import base64
 
 
 test_config = {
     "type": "service_account",
     "project_id":  os.getenv('PROJECT_ID'),
     "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-    "private_key": os.getenv('PRIVATE_KEY'),
+    "private_key": base64.b64decode(os.getenv('PRIVATE_KEY')),
     "client_email": os.getenv('CLIENT_EMAIL'),
     "client_id": os.getenv('CLIENT_ID'),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
