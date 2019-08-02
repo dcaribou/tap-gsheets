7815b0868719db98943ec628001dcde557e80d91
dcereijodo <dcereijo@digitalorigin.com>
2019-08-02 19:25:37 +0200

    handle no worksheet_name provided

--- tap_gsheets/gsheet_loader.py
+++ tap_gsheets/gsheet_loader.py
@@ -1,11 +1,26 @@
 import gspread
-from gspread.utils import numericise_all
+import warnings
+import functools
 from genson import SchemaBuilder
 from singer.schema import Schema
 from oauth2client.service_account import ServiceAccountCredentials
 import logging
 
 logging.getLogger('oauth2client').setLevel(logging.ERROR)
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+    return new_func
 
 
 class GSheetsLoader:
@@ -15,7 +30,10 @@ class GSheetsLoader:
         super(GSheetsLoader, self).__init__()
         self.config = config
         # TODO: Move the scope to a config file
-        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        scope = [
+            'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive'
+        ]
         creds = ServiceAccountCredentials.from_json_keyfile_dict(config, scope)
         client = gspread.authorize(creds)
         self.client = client
@@ -25,7 +43,7 @@ class GSheetsLoader:
         self.sheet_name = None
         self.spreadsheet = None
 
-    def get_data(self, sheet_name, worksheet_name):
+    def get_data(self, sheet_name, worksheet_name=None):
         # reset cache in case of switching to another sheet
         if self.sheet_name is None or self.sheet_name != sheet_name:
             del self.data
@@ -44,22 +62,24 @@ class GSheetsLoader:
             self.data[worksheet_name] = sheet.get_all_records()
             self.headers[worksheet_name] = sheet.row_values(1)
 
-    def get_records_as_json(self, sheet_name, worksheet_name):
-        self.get_data(sheet_name, worksheet_name)
         return self.data[worksheet_name]
 
-    def get_schema(self, sheet_name, worksheet_name):
-        self.get_data(sheet_name, worksheet_name)
+    @deprecated
+    def get_records_as_json(self, sheet_name, worksheet_name=None):
+        return self.get_data(sheet_name, worksheet_name)
+
+    def get_schema(self, sheet_name, worksheet_name=None):
+        data = self.get_data(sheet_name, worksheet_name)
 
         # add object to schema builder so he can infer schema
         builder = SchemaBuilder()
-        if len(self.data[worksheet_name]) == 0:
+        if len(data) == 0:
             # build sample record to be used for schema inference if the
             # spreadsheet is empty
             sample_record = {key: "some string" for key in self.headers[worksheet_name]}
             builder.add_object(sample_record)
         else:
-            for record in self.data[worksheet_name]:
+            for record in data:
                 builder.add_object(record)
 
         # create a singer Schema from Json Schema
